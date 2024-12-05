ff43dcfccb7427ce44e6d18922977e300ec2b36b
Shihao Zhang <15201609482@163.com>
2024-12-05 12:58:27 +0800

    changes to ch1_r1_version.py
    changes to ch1_r1_version.py due to qiskit 1.3.0

---
+++ Chapter01/ch1_r1_version_new.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  4 14:54:13 2024
+
+@author: physi
+"""
+
+#%% 
+ import qiskit
+    versions=qiskit.__version__  # 设置versions 变量
+
+#%%
+#for i in versions:
+#    print (i, versions[i])
+#%%
+for i in versions:
+    print (i)
+
+#%%
+for i in versions:
+    print (versions)
+
+#%%
+
+
